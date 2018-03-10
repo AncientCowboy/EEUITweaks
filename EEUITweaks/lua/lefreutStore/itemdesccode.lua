10e0e5bc34d5924c534351ffb813bd1d933cc92d
Renaud Durlin <renaud.durlin@gmail.com>
2018-03-11 00:03:00 +0100

    lefreut's Store Screen Tweak

--- EEUITweaks/lua/lefreutStore/itemdesccode.lua
+++ EEUITweaks/lua/lefreutStore/itemdesccode.lua
@@ -72,4 +72,31 @@ function itemDescRightButtonAction()
 		Infinity_PopMenu()
 	end
 end
+function computeSplitPosition(str, name)
+	startPos = -1
+	local firstChar = str:len() > 0 and str:byte() or 0
+	if firstChar >= 65 and firstChar <= 90 then startPos = 2
+	elseif firstChar >= 97 and firstChar <= 122 then startPos = 2
+	elseif firstChar == 195 then startPos = 3 end
+	splitPos = startPos
+	curPos = startPos
+	while splitPos ~= -1 do
+		local b = str:byte(curPos)
+		if b == nil then
+			splitPos = curPos
+		elseif b >= 240 then
+			curPos = curPos + 3
+		elseif b >= 224 then
+			curPos = curPos + 2
+		elseif b >= 194 then
+			curPos = curPos + 1
+		elseif b == 10 or b == 32 then
+			splitPos = curPos
+		end
+		Infinity_ScaleToText(name)
+		local x,y,w,h = Infinity_GetArea(name)
+		if h > 50 or curPos > str:len() then break end
+		curPos = curPos + 1
+	end
+end
 `
