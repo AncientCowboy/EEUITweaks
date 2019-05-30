9b5739af0d8c2bd5933b1407eed7eec6b309112e
Renaud Durlin <renaud.durlin@gmail.com>
2019-05-30 18:54:01 +0200

    Fix 1.3-ish Dialogue Box in confirm dialog mode

--- EEUITweaks/lua/lefreut1_3ishDialog/WDConfirmCodeBG.lua
+++ EEUITweaks/lua/lefreut1_3ishDialog/WDConfirmCodeBG.lua
@@ -1,5 +1,17 @@
 `
 function getDialogButtonEnabled()
-	return (#worldPlayerDialogChoices == 0)
+	if(gameOptions.m_bConfirmDialog == true) then
+		return true
+	else
+		return (#worldPlayerDialogChoices == 0)
+	end
+end
+
+function getDialogButtonClickable()
+	if(gameOptions.m_bConfirmDialog == true) then
+		return (#worldPlayerDialogChoices == 0) or (worldPlayerDialogSelection and worldPlayerDialogSelection > 2) --no choices, or we've selected a choice.
+	else
+		return true
+	end
 end
 `
