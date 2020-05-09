2b21f42b1bd1e24ac339dad1dd4d3f1dc83ecc34
Renaud Durlin <renaud.durlin@gmail.com>
2020-05-09 17:01:29 +0200

    Better compatibility between Mod Options/Hidden Options and LeUI BG1EE/SoD

--- EEUITweaks/lua/ModsOptions/uimenutop1.lua
+++
@@ -1,9 +0,0 @@
-
-if(uiTranslationFile) then
-	Infinity_DoFile(string.sub(uiTranslationFile,1,3).."ModOp.lua")
-	if(uiStrings.MODOPT_TITLE == nil) then
-		Infinity_DoFile("en_ModOp.lua")
-	end
-else
-	Infinity_DoFile("en_ModOp.lua")
-end
